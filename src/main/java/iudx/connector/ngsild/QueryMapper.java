8ca11c3c341dde5a7b4a8def1acb7650e462b993
Kailash Adhikari <adhikari.kailash88@gmail.com>
2020-03-16 09:56:14 +0530

    Update QueryMapper.java
    changes for issues,
    1) ID in all requests,
    2) Complex queries, attribute-filter etc

--- src/main/java/iudx/connector/ngsild/QueryMapper.java
+++ src/main/java/iudx/connector/ngsild/QueryMapper.java
@@ -3,15 +3,14 @@ package iudx.connector.ngsild;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.security.Identity;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import io.vertx.core.MultiMap;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public class QueryMapper {
@@ -19,7 +18,8 @@ public class QueryMapper {
 	public JsonObject getIUDXQuery(MultiMap paramsMap) {
 		JsonObject rootNode = new JsonObject();
 		paramsMap.forEach(entry -> {
-			if(entry.getKey().equals("q")) return;
+			if (entry.getKey().equals("q"))
+				return;
 			rootNode.put(NGSI2IUDXMapping.valueOf(entry.getKey()).getValue(),
 					this.mapperDataTypeHelper(entry.getKey(), entry));
 		});
@@ -47,49 +47,78 @@ public class QueryMapper {
 				rootNode.put("relation", relation);
 			}
 		}
-		
-		if(paramsMap.contains("q")) {
-			String qFilter=paramsMap.get("q");
-			String[] options=qFilter.split(";");
-			Arrays.stream(options).forEach(e->{
-				List<String> queryTerms=getQueryTerms(e);
+
+		if (paramsMap.contains("q")) {
+			String qFilter = paramsMap.get("q");
+			String[] options = qFilter.split(";");
+			Arrays.stream(options).forEach(e -> {
+				List<String> queryTerms = getQueryTerms(e);
 				rootNode.put("attribute-name", queryTerms.get(0));
-				rootNode.put("comparison-operator", QueryOperators.getName4Value( queryTerms.get(1)));
+				rootNode.put("comparison-operator",
+						QueryOperators.getName4Value(queryTerms.get(1)));
 				rootNode.put("attribute-value", queryTerms.get(2));
 			});
 		}
-		
+
+		if (rootNode.containsKey("geometry") && rootNode.containsKey("coordinates")) {
+			String coordinates = rootNode.getString("coordinates").replaceAll("\\[|\\]",
+					"");
+			String geomType = rootNode.getString("geometry");
+			if (geomType.equalsIgnoreCase("Polygon")) {
+				String geom = geomType + "((" + coordinates + "))";
+				rootNode.put("geometry", geom);
+				rootNode.remove("coordinates");
+			}
+			else if (geomType.equalsIgnoreCase("LineString")
+					|| geomType.equalsIgnoreCase("MultiLineString")) {
+				String geom = geomType + "(" + coordinates + ")";
+				rootNode.put("geometry", geom);
+				rootNode.remove("coordinates");
+			}
+			else if (geomType.equalsIgnoreCase("point")) {
+				//handle probable circle geom here.
+			}
+		}
+
 		System.out.println(rootNode);
 		return rootNode;
 	}
-	
+
 	private List<String> getQueryTerms(String queryTerms) {
-		List<String> qTerms=new ArrayList<String>();
-		int length=queryTerms.length();
-		int startIndex=0;
-		boolean specialCharFound=false;
-		for(int i=0;i<length;i++) {
-			Character c=queryTerms.charAt(i);
-			if(!(Character.isLetter(c) || Character.isDigit(c)) && !specialCharFound) {
+		List<String> qTerms = new ArrayList<String>();
+		int length = queryTerms.length();
+		int startIndex = 0;
+		boolean specialCharFound = false;
+		for (int i = 0; i < length; i++) {
+			Character c = queryTerms.charAt(i);
+			if (!(Character.isLetter(c) || Character.isDigit(c)) && !specialCharFound) {
 				qTerms.add(queryTerms.substring(startIndex, i));
-				startIndex=i;
-				specialCharFound=true;
-				
-			}else {
-				if(specialCharFound && (Character.isLetter(c) || Character.isDigit(c))) {
+				startIndex = i;
+				specialCharFound = true;
+
+			}
+			else {
+				if (specialCharFound && (Character.isLetter(c) || Character.isDigit(c))) {
 					qTerms.add(queryTerms.substring(startIndex, i));
 					qTerms.add(queryTerms.substring(i));
 					break;
 				}
 			}
-			
+
 		}
 		return qTerms;
 	}
 
 	private Object mapperDataTypeHelper(String key, Map.Entry<String, String> entry) {
-		if (key.equalsIgnoreCase("id") || key.equalsIgnoreCase("attrs")) {
+		if (key.equalsIgnoreCase("id")) {
 			return entry.getValue();
+		}
+		else if (key.equalsIgnoreCase("attrs")) {
+			JsonArray array = new JsonArray();
+			List<String> list = Arrays.stream(entry.getValue().split(","))
+					.collect(Collectors.toList());
+			list.forEach(s -> array.add(s));
+			return array;
 		}
 		else if (key.equalsIgnoreCase("geometry")) {
 			return entry.getValue();
@@ -107,9 +136,6 @@ public class QueryMapper {
 		else if (key.equals("timerel")) {
 			return entry.getValue().toString();
 		}
-		/*
-		 * else if(key.equalsIgnoreCase("q")) { return null; }
-		 */
 		else {
 			return entry.getValue();
 		}
@@ -123,7 +149,7 @@ enum NGSI2IUDXMapping {
 	type("resource-server-id"),
 	coordinates("coordinates"),
 	geometry("geometry"),
-	timerel("trelation"),
+	timerel("TRelation"),
 	georel("relation"),
 	endtime("time"),
 	time("time"),
@@ -146,7 +172,7 @@ enum GeoRelationMapping {
 	within("within"),
 	contains("contains"),
 	overlaps("overlaps"),
-	intersect("intersect"),
+	intersects("intersects"),
 	equal("equal"),
 	disjoint("disjoint");
 
@@ -161,17 +187,16 @@ enum GeoRelationMapping {
 	}
 }
 
-enum QueryOperators{
-	
-	PropertyIsEqualTo("=="),
-	PropertyIsNotEqualTo("!="),
-	PropertyIsGreaterThan(">"),
-	PropertyIsLessThan("<"),
-	PropertyIsLesThanOrEqualTo("<="),
-	PropertyIsGreaterThanOrEqualTo(">="),
-	PropertyIsLike("like");
-	
-	
+enum QueryOperators {
+
+	propertyisequalto("=="),
+	propertyisnotequalto("!="),
+	propertyisgreaterthan(">"),
+	propertyislessthan("<"),
+	propertyislessthanorequalto("<="),
+	propertyisgreaterthanorequalto(">="),
+	propertyislike("like");
+
 	private final String value;
 
 	QueryOperators(String value) {
@@ -181,11 +206,10 @@ enum QueryOperators{
 	public String getValue() {
 		return value;
 	}
-	
-	
+
 	public static String getName4Value(String value) {
-		for(QueryOperators element:QueryOperators.values()) {
-			if(element.value.equalsIgnoreCase(value)) {
+		for (QueryOperators element : QueryOperators.values()) {
+			if (element.value.equalsIgnoreCase(value)) {
 				return element.name();
 			}
 		}
